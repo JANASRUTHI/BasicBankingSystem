519320e968f3012e96595d2f75ff028c4d62720b
JANASRUTHI <73476001+JANASRUTHI@users.noreply.github.com>
2021-08-15 19:25:45 +0530

    Add files via upload

---
+++ js/banking .js
@@ -0,0 +1,16 @@
+function Home()
+{
+    window.location.href = "home.html";
+}
+function ContactusPage() 
+{
+    window.location.href = "Contactus.html";
+}
+function ViewCustomers()
+{
+    window.location.href = "view-customers.php";
+}
+function ViewTransactions()
+{
+    window.location.href = "view-transaction.php";
+}
