b56ba1bfd37b5edebb3f8bdc30743d42385c6e92
JANASRUTHI <73476001+JANASRUTHI@users.noreply.github.com>
2021-08-15 19:08:46 +0530

    Add files via upload

---
+++ navbar.php
@@ -0,0 +1,19 @@
+<!-- navbar -->
+<nav class="navbar navbar-expand-md navbar-black bg-black">
+  <a class="navbar-brand" href="index.html" style="color:white"><b>JOO BANK</b></a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="collapsibleNavbar">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item">
+        <a class="nav-link" href="index.html" style="color:white"><b>HOME</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="view-customers.php" style="color:white"><b>TRANSFER MONEY</b></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="transactionhistory.php" style="color:white"><b>TRANSFER HISTORY</b></a>
+      </li>
+  </div>
+</nav>
